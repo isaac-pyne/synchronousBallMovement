883763760b708df53d4076ad0f9f7377425021ea
isaac-pyne <popcorngames2008@gmail.com>
2021-03-28 16:44:11 -0600

    syncrones ball mouvment

--- sketch.js
+++ sketch.js
@@ -1,29 +1,59 @@
-var ball;
+var hypnoticBall;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+
+    //Everytime a change in the database values of position (reference) happens, the readPosition function is called.
+    //If there is any error in reading the values in the database, the showError function is called.
+    hypnoticBallPosition.on("value", readPosition, showError);
+
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+
+    if(position!== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+    
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x ,
+        'y': position.y + y
+      })
+}
+
+
+function readPosition(data){
+    position = data.val();
+    console.log(position.x);
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+
+function showError(){
+    console.log("Error in writing to the database");
 }
